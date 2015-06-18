6e5f7c2b4d5da0bdc7f2d199a76b280933bb1af8
Christopher D. Long <galizur@gmail.com>
2015-06-18 00:27:51 -0400

    Princeton Election Consortium code.

---
+++ pec/Senate_November_prediction_6sep.m
@@ -0,0 +1,36 @@
+Senate_history=load('Senate_estimate_history.csv');
+d=Senate_history(:,1);
+mm=Senate_history(:,12);
+
+if today>=datenum('03-Sep-2014')
+    Orman_offset=0.47
+else
+    Orman_offset=0
+end
+% Orman offset is the shift in Meta-Margin caused by Kansas change
+% An offset of 0.47 assumes a tie in KS-Sen on Election Day
+% An offset of 0.83 assumes Orman leads by 1 sigma on Election Day
+% calculated by Sam Wang based on June-September 2014 polls
+
+systematic=0.7; % Guess at systematic error this year
+
+blackswanfactor=1; % a 30% probability of >2 sigma event; allows the fundamentals-type argument
+
+%%%%% Calculate November prediction based on Meta-Margin history
+[C,IA,IC] = unique(d,'stable'); % Find unique entries in the history
+mean_mm=mean(mm(IA(find(d(IA)>=153))))+Orman_offset; % This is the long-term prediction...
+sd_mm=std(mm(IA(find(d(IA)>=153)))); % ...based on data after June 1 (Julian 153)
+sd_mm=sqrt(sd_mm+Orman_offset*Orman_offset); % stuff happens between now and November
+h=datenum('04-Nov-2014')-today; % days until election (note: November 4, Julian 309)
+
+if and(h<=35,h>0) % election is soon, so combine current and long-term
+    current_mm=mm(max(find(d==max(d)))); % Find the most recent Meta-Margin
+    predicted_mm=(mean_mm*h+current_mm*(35-h))/35;
+    predicted_sd=sqrt((sd_mm*h/35)^2+systematic^2);
+    D_November_control_probability = 100*tcdf(predicted_mm/predicted_sd,blackswanfactor) 
+else % election is far off, so use the long-term prediction
+    predicted_sd=sqrt(sd_mm^2+systematic^2);
+    D_November_control_probability = 100*tcdf((mean_mm)/predicted_sd,blackswanfactor) 
+end
+dlmwrite('Senate_D_November_control_probability.csv',[today-datenum('31-Dec-2013') D_November_control_probability])
+%%%%% end November prediction calculation
